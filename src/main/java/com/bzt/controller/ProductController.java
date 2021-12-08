0d8a9f87df6ad8bdd0119ff4ff3752a63559ec57
angelobezutti <angeloantoniobzt@gmail.com>
2021-12-08 11:52:09 -0300

    Implements LIST and GET BY id

--- src/main/java/com/bzt/controller/ProductController.java
+++ src/main/java/com/bzt/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bzt.controller.dto.ProductDto;
 import com.bzt.model.Product;
 import com.bzt.repository.ProductRepository;
 
@@ -22,7 +24,15 @@ public class ProductController {
 	
 	@GetMapping
 	public List<Product> listProduct(){
-			return productRepository.findAll();
+		return productRepository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public ProductDto detailsProduct(@PathVariable Long id) {
+		 Product product = productRepository.getById(id);
+	      return new ProductDto(product);
+	}
+	
+	
 	
 }
