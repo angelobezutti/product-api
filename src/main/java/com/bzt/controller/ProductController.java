bd846948a9a0e50e99a792dd0c1692d204f21d08
angelobezutti <angeloantoniobzt@gmail.com>
2021-12-08 12:53:29 -0300

    Implementation POST

--- src/main/java/com/bzt/controller/ProductController.java
+++ src/main/java/com/bzt/controller/ProductController.java
@@ -1,12 +1,20 @@
 package com.bzt.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.bzt.controller.dto.ProductDto;
 import com.bzt.model.Product;
@@ -33,6 +41,14 @@ public class ProductController {
 	      return new ProductDto(product);
 	}
 	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<ProductDto> createProduct(@RequestBody @Valid Product product, UriComponentsBuilder uriBuilder) {
+		productRepository.save(product);
+		
+		URI uri = uriBuilder.path("/products/{id}").buildAndExpand(product.getId()).toUri();
+		return ResponseEntity.created(uri).body(new ProductDto(product));
+	}	
 	
 	
 }
